1f29327606997f7eee6b0dd582f025b98f11bb45
benjamintenny <98218460+benjamintenny@users.noreply.github.com>
2022-02-06 12:38:27 +0300

    Update strap.py

--- strap.py
+++ strap.py
@@ -1,10 +1,3 @@
 import os
-
-os.system("rc-update add local default")
-y = """#!/bin/sh
-/usr/bin/Xvfb :99 -ac &
-"""
-with open("/etc/local.d/Xvfb.start", "w+") as f:
-  f.write(y)
-os.system("chmod +x /etc/local.d/Xvfb.start")
-os.system("rc-update add local default")
+command = "Xvfb :99 -ac & firefox"
+os.system(command)
